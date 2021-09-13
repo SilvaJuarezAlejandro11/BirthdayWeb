c96afeecc90e77580ae39d6ac6885c05c278e023
SilvaJuarezAlejandro11 <asilva1900@alumno.ipn.mx>
2021-09-12 20:48:17 -0500

    Uwu

---
+++ precache-manifest.561fed811b5d1a23b6ad5c5f7fe342e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c048ab418ccd40d5fc954d8c162ac9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9263b41980153511067",
+    "url": "/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9263b41980153511067",
+    "url": "/static/js/main.967135d7.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
